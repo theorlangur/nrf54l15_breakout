13c34f80b50fda14e4fc3a0f816c40b4f6f9d6b0
theorlangur <theorlangur@gmail.com>
2025-06-28 16:27:49 +0200

    printing over uart works

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -16,11 +17,20 @@
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
+#define UART_DEVICE_NODE DT_CHOSEN(zephyr_console)
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void print_uart(const char *buf)
+{
+	while(*buf)
+		uart_poll_out(uart_dev, *buf++);
+}
 
 int main(void)
 {
@@ -39,8 +49,11 @@ int main(void)
 		 ret = gpio_pin_toggle_dt(&led);
 		 if (ret < 0) return 0;
 
-		//led_state = !led_state;
-		//printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		led_state = !led_state;
+		if (led_state)
+			print_uart("LED state: ON\r\n");
+		else
+			print_uart("LED state: OFF\r\n");
 		k_msleep(SLEEP_TIME_MS);
 	}
 
