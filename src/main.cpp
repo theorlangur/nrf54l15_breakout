da0acac460fa97f2ba99acf96875f31bf39e2568
theorlangur <theorlangur@gmail.com>
2025-06-28 15:30:58 +0200

    a working version for my prototype bm54l15 board

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   5000
+#define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -27,24 +27,20 @@ int main(void)
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+	 if (!gpio_is_ready_dt(&led)) {
+	 	return 0;
+	 }
+	 ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	 if (ret < 0) {
+	 	return 0;
+	 }
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) return 0;
-		k_msleep(1000);
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) return 0;
+		 ret = gpio_pin_toggle_dt(&led);
+		 if (ret < 0) return 0;
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		//led_state = !led_state;
+		//printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 
